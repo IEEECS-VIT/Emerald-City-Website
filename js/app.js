464bfcdc1062f8167f91d31460d6247a03c1f988
Amrit Sundarka <amritsundarka@gmail.com>
2024-01-19 12:09:49 +0530

    Fixed Arrow Icon Animations

--- js/app.js
+++ js/app.js
@@ -1,32 +1,33 @@
-
 function toggleAnswer(element) {
-    var answer = element.nextElementSibling;
-    var arrowIcon = element.querySelector('.arrow-icon');
-  
-    if (answer.style.display === 'block') {
-        answer.style.animation = 'fadeOutUp 0.5s ease-out';
-        arrowIcon.style.transform = 'rotate(0deg)';
-        setTimeout(function () {
-            answer.style.display = 'none';
-            answer.style.animation = '';
-        }, 500);
-    } else {
-        answer.style.display = 'block';
-        answer.style.animation = 'fadeInDown 0.5s ease-out';
-        arrowIcon.style.transform = 'rotate(180deg)';
-    }
+  var answer = element.nextElementSibling;
+  var arrowIcon = element.querySelector('.arrow-icon');
+
+  if (answer.style.display === 'block') {
+      answer.style.animation = 'fadeOutUp 0.5s ease-out';
+      arrowIcon.style.transform = 'rotate(0deg)';
+      element.classList.remove('open');
+      setTimeout(function () {
+          answer.style.display = 'none';
+          answer.style.animation = '';
+      }, 500);
+  } else {
+      answer.style.display = 'block';
+      answer.style.animation = 'fadeInDown 0.5s ease-out';
+      arrowIcon.style.transform = 'rotate(180deg)';
+      element.classList.add('open');
   }
-  
-  var questionElements = document.querySelectorAll('.question');
-  questionElements.forEach(function(element) {
-      element.addEventListener('click', function() {
-          toggleAnswer(this);
-      });
+}
+
+var questionElements = document.querySelectorAll('.question');
+questionElements.forEach(function (element) {
+  element.addEventListener('click', function () {
+      toggleAnswer(this);
   });
+});
 
 
   document.getElementById('toggleLink').addEventListener('click', function() {
-    var sidebar = document.querySelector('.sidebar'); 
+    var sidebar = document.querySelector('.sidebar');
 
     if (sidebar.style.display === 'none' || sidebar.style.display === '') {
         sidebar.style.display = 'flex';
