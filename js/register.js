3f8b119892a3e1c1c599b7eb09de554f8d5b5eb4
Ansh Mehta <anshkapilmehta@gmail.com>
2024-01-23 00:26:05 +0530

    fixes, back button, animation

--- js/register.js
+++ js/register.js
@@ -1,21 +1,38 @@
-let leaderpage=true;
+let leaderPage=true;
+
+function animateSwitch() {
+    if (leaderPage) {
+        gsap.to("#leaderimage", {opacity: 0.5, duration: 1});
+        gsap.to("#memberimage", {opacity: 1, duration: 1});
+    } else {
+        gsap.to("#leaderimage", {opacity: 1, duration: 1});
+        gsap.to("#memberimage", {opacity: 0.5, duration: 1});
+    }
+}
 
 document.getElementById("leaderimage").addEventListener("click", function() {
     document.getElementById("leaderimage").src = "../img/register/teamleaderactive.svg";
     document.getElementById("memberimage").src = "../img/register/teammemberinactive.svg";
-    console.log("leader");
     const teamdetailslabel = document.getElementById("teamdetailslabel");
     teamdetailslabel.innerHTML = "Team Name";
     const joinbtn = document.getElementById("joinbtn");
     joinbtn.src = "../img/register/registerteamleader.svg";
+
+    animateSwitch();
+    leaderPage = true;
+
 });
 
 document.getElementById("memberimage").addEventListener("click", function() {
     document.getElementById("leaderimage").src = "../img/register/teamleaderinactive.svg";
     document.getElementById("memberimage").src = "../img/register/teammemberactive.svg";
-    console.log("member");
     const teamdetailslabel = document.getElementById("teamdetailslabel");
     teamdetailslabel.innerHTML = "Team Referral Code";
     const joinbtn = document.getElementById("joinbtn");
     joinbtn.src = "../img/register/jointeam.svg";
-});
+
+    animateSwitch();
+    leaderPage = false;
+}
+);
+
